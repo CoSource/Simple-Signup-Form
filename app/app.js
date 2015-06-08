59b8b191b4a26d5340b8c82686defb05e282fc49
cosource <bailey.richard@gmail.com>
2015-06-08 18:10:13 +0200

    Initial Release

---
+++ app/app.js
@@ -0,0 +1,9 @@
+﻿angular.module('signup', [])
+    .directive('csSignupForm', function () {
+        return {
+            templateUrl: 'app/signon/registerTemplate.html',
+            restrict: 'AE',
+            replace: true,
+            transclude: true
+        }
+    });
